8ee07a6bde2698bf6ef0e660ce6f997799686587
Sean <seanwang.sydney@gmail.com>
2025-11-11 10:30:38 +1100

    Practice creation

---
+++ src/weeks/week6/practice.tsx
@@ -0,0 +1,9 @@
+function Week6Practice({ goBack }: { goBack: () => void }) {
+    return (
+        <div>
+            <button className='bottom-back-button' onClick={goBack}>← Back</button>
+        </div>
+    )
+}
+
+export default Week6Practice;
