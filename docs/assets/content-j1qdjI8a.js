5a3d37ca2cfb2552f21df79ba17f9e59bc151e7a
Sean <seanwang.sydney@gmail.com>
2025-11-11 16:02:04 +1100

    Latest build

---
+++ docs/assets/content-j1qdjI8a.js
@@ -0,0 +1,1 @@
+import{r as e,t}from"./index-yWttgpuy.js";var n=e(t());function r({goBack:e}){return(0,n.jsx)(`div`,{children:(0,n.jsx)(`button`,{onClick:e,children:`← Back`})})}var i=r;export{i as default};
